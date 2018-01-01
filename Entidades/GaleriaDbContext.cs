67e2a32e70a8da2fd04b4bac4b2e89f672934211
David Ali Zambrano Burgos <davazamb10@outlook.com>
2018-01-01 10:10:09 -0300

    Cambios migraciones

---
+++ Entidades/GaleriaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entidades.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class GaleriaDbContext : DbContext
+    {
+        public GaleriaDbContext(DbContextOptions<GaleriaDbContext>options) : base(options)
+        {                                                                                   
+
+        }
+        public DbSet<Album> Album { get; set; } 
+    }
+}
